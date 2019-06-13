7ed52db9900b5d81c3dec67b3cb788b1e72f3e02
Martin Bickel <martin.mb.bickel@gmx.de>
2019-06-13 17:03:14 +0200

    Inserting command to read the source-file

--- file_converter.py
+++ file_converter.py
@@ -2,16 +2,25 @@ import time
 import pandas as pd
 import numpy as np
 
+CITY_DATA = '/Users/acemartinb/Documents/Projekte_Python/convert_to_files/test_file.csv'
+
+def load_data(CITY_DATA):
+    """
+    Loads the source file.
+
+    Args:
+
+    Returns:
+        df - Pandas DataFrame containing the data from the .csv
+    """
+    df = pd.read_csv(CITY_DATA)
+
+    return df
+
 
 def main():
     while True:
-        city, month, day = get_filters()
-        df = load_data(city, month, day)
-
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
+        df = load_data()
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
